9016c516d758c381c525566df3f16171096e9629
Farzana <49375947+FarzanaR11@users.noreply.github.com>
2021-04-17 20:48:09 +0600

    Add files via upload

---
+++ css/style.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		*{margin: 0; padding: 0; font-family: 'Muli', sans-serif; box-sizing: border-box;}
+		
+	</style>
+</head>
+
+</html>
